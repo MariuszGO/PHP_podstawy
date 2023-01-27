2f2b216952b67592849ecef81a92e835b2eea57a
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2023-01-27 11:41:29 +0100

    Revert "v1"
    This reverts commit bf0f82794bbca10298ad3b1cdfdaaa76acde2761.

--- kolor.php
+++ kolor.php
@@ -1,37 +1,10 @@
 <?php
 
-
-
-
-if( $_GET['x'] == "Losuj" ){
-
-    echo '<a href="kolor.html">kolor.html</a><br>';
-$r= rand(1,255);
-$g= rand(1,255);
-$b= rand(1,255);
-
-echo '    <form action="kolor.php" method="get">
-
-<input type="submit" value="Losuj" name="x">
-<br>
-<br>
-
-
-
-</form>
-';
-
-
-}
-else{
-    $r = $_GET['r'];
+$r = $_GET['r'];
 $g = $_GET['g'];
 $b = $_GET['b'];
-}
 
 $kolor =  "background-color:rgb($r,$g,$b)";
-
-
 
 echo "<body style=$kolor>";
 
@@ -40,11 +13,4 @@ echo "R: " . $r . " G: " . $g . " B: " . $b ."<br>";
 
 echo "</body>";
 
-
 ?>
-
-
-    
-
-
-    </form>
